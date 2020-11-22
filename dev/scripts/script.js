894a7e89e7e79b763afcb0ebbe9699872bd78b58
Seraphina White <seraphinawhite.6@gmail.com>
2020-11-21 19:02:48 -0800

    updates

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,9 @@
 // Write your JavaScript here...
+function myFunction() {
+    var x = document.getElementsByClassName("nav-Links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
