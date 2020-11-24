2eafb4de98729e8b0fea383b40c1defcc6d5d6f8
Seraphina White <seraphinawhite.6@gmail.com>
2020-11-23 22:46:29 -0800

    updates

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,9 +1,15 @@
 // Write your JavaScript here...
 function myFunction() {
-    var x = document.getElementsByClassName("nav-links");
+    var x = document.getElementById("nav-links");
+    var y = document.getElementById("search")
+    var z = document.getElementById("socials")
     if (x.style.display === "block") {
       x.style.display = "none";
+      y.style.display = "none";
+      z.style.display = "none";
     } else {
       x.style.display = "block";
+      y.style.display = "block";
+      z.style.display = "block";
     }
   }
